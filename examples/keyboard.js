1d09b6c1614d9d1117bd7e28fb6a3688f63b25de
Rud <rudolph.sand@gmail.com>
2013-06-23 21:44:53 +0200

    temp fix examples/keyboard.js

--- examples/keyboard.js
+++ examples/keyboard.js
@@ -1,16 +1,12 @@
+var prime = require('prime');
 var midigrid = require('../lib/midigrid.js');
 
-// create a midigrid device
-var device = midigrid.create({
-  serialoscId: 'keyboardgrid',
-  name: 'monome 64 (k0000001)',
-  prefix: '/keyboardgrid',
-  // set these to valid midi devices, use listmidi.js to see what you have
-  midiIn: 'LPK25',
-  midiOut: 'IAC Driver IAC Bus 2',
-
-  // this should map a midi note number to an x/y coordinate
-  serialoscMapFunc: function(noteNum) {
+var testDevice = prime({
+  inherits: midigrid,
+  constructor: function(options) {
+    midigrid.prototype.constructor.call(this, options);
+  },
+  _serialoscMapFunc: function(noteNum) {
     var x = noteNum % device.sizeX;
     var y = Math.floor(noteNum / device.sizeX);
     console.log('press event: converted note ' + noteNum + ' to ' + x + ', ' + y)
@@ -18,23 +14,32 @@ var device = midigrid.create({
   },
 
   // this should map an x/y coordinate to a midi note number
-  midiMapFunc: function(data) {
+  _midiMapFunc: function(data) {
     var noteNum = data.x + (data.y * device.sizeX);
     console.log('led event: converted ' + data.x + ', ' + data.y + ' to note ' + noteNum);
     return noteNum;
   },
 
   // the velocity to use when turning a led on
-  velocityOn: function(data) {
+  _velocityOn: function(data) {
     return 127;
   },
   
   // the velocity to use when turning a led off
-  velocityOff: function(data) {
+  _velocityOff: function(data) {
     return 0;
   }
 });
 
+var options = {
+  serialoscId: 'keyboardgrid',
+  name: 'monome 64 (k0000001)',
+  prefix: '/keyboardgrid',
+  // set these to valid midi devices, use listmidi.js to see what you have
+  midiIn: 'Akai APC40',
+  midiOut: 'Gestionnaire IAC Bus IAC 1'
+};
+var device = new testDevice(options);
 device.start();
 
 var stdin = process.openStdin();
